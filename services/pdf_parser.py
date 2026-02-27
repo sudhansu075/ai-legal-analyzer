557d0e3875d443fa10337273ecc4f959d8e3ad90
Behera <sudhansu.a.behera@accenture.com>
2026-02-27 13:01:12 +0200

    deleting old doc parser

--- services/pdf_parser.py
+++
@@ -1,8 +0,0 @@
-from pypdf import PdfReader
-
-def extract_text_from_pdf(file):
-    reader = PdfReader(file)
-    text = ""
-    for page in reader.pages:
-        text += page.extract_text() + "\n"
-    return text
